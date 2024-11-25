f375a420fac00edb5ae4c6b2ab4df2538b9d9aca
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-11-25 16:04:21 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Project start!</li>
+          <li>Project start!!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
