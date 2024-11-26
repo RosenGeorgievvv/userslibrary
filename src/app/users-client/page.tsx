406f7b5c7c349b9e4faedfa9ca54b50c0003af86
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-11-26 17:18:52 +0200

    Update page.tsx

--- src/app/users-client/page.tsx
+++ src/app/users-client/page.tsx
@@ -7,3 +7,28 @@ type User = {
     email: string;
     phone: string;
 }
+
+export default function UsersClient(){
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        async function fetchUsers(){
+            try{
+                const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                if(!response.ok) throw new Error("Failed to fetch users");
+                const data = await response.json();
+                setUsers(data);
+            }catch(err){
+                setError('Failed to fetch users');
+                if(err instanceof Error){
+                    setError(`Failed to fetch users: ${err.message}`);
+                }
+            } finally{
+                setLoading(false);
+            }
+        }
+        fetchUsers()
+    }, []);
+}
