89076059810ff83e0ed8771f15c856b3864f8832
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-12-04 17:18:42 +0200

    Update counter.tsx

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export const Counter = () => {
     console.log("Counter component");
     const [count, setCount] = useState(0);
-  
+   
     return (
         <button onClick={() => setCount(count + 1)}>CLicked {count} times</button>
     )
